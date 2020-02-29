3cc946f771cb707d184bc8d8cc4887ab35cd6904
Alifudin A <alifudinalfarizi@gmail.com>
2020-03-01 00:35:48 +0700

    commit

---
+++ app/src/main/java/com/divierda/msgshareapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.divierda.msgshareapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener{
+            Log.i("Main Activity", "Button was clicked !")
+            Toast.makeText(this, "Button was clicked !",  Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener{
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("user_message", message)
+            startActivity(intent)
+        }
+
+        btnShareToOtherApps.setOnClickListener{
+            val message: String = etUserMessage.text.toString()
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Select application : "))
+        }
+
+        btnRecyclerViewDemo.setOnClickListener {
+            val intent = Intent(this, HobbiesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/divierda/msgshareapp/Model.kt
@@ -0,0 +1,43 @@
+package com.divierda.msgshareapp
+
+data class Hobby(var title: String)
+
+object Supplier {
+    val hobbies = listOf<Hobby>(
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Sleeping"),
+        Hobby("Eating"),
+        Hobby("Watching"),
+        Hobby("Browsing"),
+        Hobby("Drawing"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Sleeping"),
+        Hobby("Eating"),
+        Hobby("Watching"),
+        Hobby("Browsing"),
+        Hobby("Drawing"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Sleeping"),
+        Hobby("Eating"),
+        Hobby("Watching"),
+        Hobby("Browsing"),
+        Hobby("Drawing"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Sleeping"),
+        Hobby("Eating"),
+        Hobby("Watching"),
+        Hobby("Browsing"),
+        Hobby("Drawing"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Sleeping"),
+        Hobby("Eating"),
+        Hobby("Watching"),
+        Hobby("Browsing"),
+        Hobby("Drawing")
+    )
+}
